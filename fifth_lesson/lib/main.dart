dd7da6f707ac4a4a141de1b785c7bf10fe10f4d8
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2022-11-22 16:09:04 +0300

    all is work

--- fifth_lesson/lib/main.dart
+++ fifth_lesson/lib/main.dart
@@ -1,115 +1,264 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+
+  // Future(() {
+  //   scheduleMicrotask(() => print("before loop 1"));
+  //   print("in event loop");
+  //   PaintingBinding.instance.imageCache
+  //       .evict("https://pbs.twimg.com/media/Eooss15XUAE2Ed8.jpg");
+  // });
+}
+
+mixin CachedNetworkImage {
+  static evictFromCache(String url) {}
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<BottomNavigationBarItem> items = [
+    const BottomNavigationBarItem(
+        icon: Icon(Icons.photo_album_rounded), label: "Photo"),
+    const BottomNavigationBarItem(
+        icon: Icon(Icons.chat_rounded), label: "Chat"),
+    const BottomNavigationBarItem(
+        icon: Icon(Icons.account_box_rounded), label: "Profile")
+  ];
 
-  void _incrementCounter() {
+  static const List<Widget> _barContent = <Widget>[
+    Text(
+      'PHOTO',
+    ),
+    Text(
+      'CHAT',
+    ),
+    Text(
+      'PROFILE',
+    ),
+  ];
+
+  static Map<String, Icon> drawerItems = {
+    "Home": const Icon(Icons.home),
+    "Profile": const Icon(Icons.person),
+    "Images": const Icon(Icons.image)
+  };
+  ListView test = ListView.builder(
+    scrollDirection: Axis.vertical,
+    shrinkWrap: true,
+    itemCount: drawerItems.length,
+    itemBuilder: (context, index) {
+      return ListTile(
+        leading: drawerItems.values.elementAt(index),
+        title: Text('${drawerItems.keys.elementAt(index)}'),
+        trailing: const Icon(Icons.arrow_right),
+      );
+    },
+  );
+
+  int currentIndex = 0;
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      currentIndex = index;
+      switch (currentIndex) {
+        case 0:
+          const Text("wdscsdcsdc");
+          break;
+      }
+      print(currentIndex);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              Builder(
+                builder: (context) => IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: () => Scaffold.of(context).openEndDrawer(),
+                  tooltip:
+                      MaterialLocalizations.of(context).openAppDrawerTooltip,
+                ),
+              ),
+            ],
+            title: Text(widget.title),
+            bottom: const TabBar(tabs: [
+              Tab(
+                text: "First",
+              ),
+              Tab(
+                text: "Second",
+              ),
+              Tab(
+                text: "Third",
+              )
+            ]),
+          ),
+          endDrawer: Drawer(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                      radius: 60,
+                      backgroundImage: NetworkImage(
+                          "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg")),
+                  Text("Артем Ильинский"),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: CircleAvatar(
+                      radius: 60,
+                      backgroundImage: NetworkImage(
+                          "http://www.rosphoto.com/images/u/articles/1510/7_5.jpg")),
+                ),
+                test,
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(22.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MaterialButton(
+                        padding: EdgeInsets.all(18),
+                        color: Colors.lightBlue,
+                        onPressed: () {
+                          print("object");
+                        },
+                        child: Text(
+                          "Выход",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Spacer(),
+                      MaterialButton(
+                        padding: EdgeInsets.all(18),
+                        color: Colors.lightBlue,
+                        onPressed: () {
+                          print("object");
+                        },
+                        child: Text("Регистрация",
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: items,
+            currentIndex: currentIndex,
+            onTap: _onItemTapped,
+          ),
+          body: Center(child: _barContent.elementAt(currentIndex)),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print("tapped");
+                showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SizedBox(
+                          height: 180,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                        child: Container(
+                                      child: const Icon(Icons.person),
+                                      alignment: Alignment.topLeft,
+                                    )),
+                                    Expanded(
+                                        flex: 3,
+                                        child: Container(
+                                          child: const Text("Сумма"),
+                                        )),
+                                    Expanded(
+                                        child: Container(
+                                      child: const Text(
+                                        "200 руб,",
+                                        textAlign: TextAlign.end,
+                                      ),
+                                    )),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(30.0),
+                                child: Center(
+                                  child: MaterialButton(
+                                    onPressed: () {
+                                      print("tapped");
+                                    },
+                                    color: Colors.amber,
+                                    child: const Text("button"),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ));
+                    });
+              },
+              child: const Icon(Icons.sim_card_alert)),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.miniEndFloat,
+          // body: const Center(
+          //   child: TabBarView(
+          //     children: <Widget>[
+          //       Text(
+          //         'First',
+          //         textAlign: TextAlign.center,
+          //       ),
+          //       Text('Second', textAlign: TextAlign.center),
+          //       Text('Third', textAlign: TextAlign.center),
+          //     ],
+          //   ),
+          // ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
