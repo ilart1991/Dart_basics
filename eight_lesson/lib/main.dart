d005601b8cf99bf589fd06ca373fcd6890112d38
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2022-11-30 14:44:12 +0300

    completed

--- eight_lesson/lib/main.dart
+++ eight_lesson/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'my_app.dart';
+import 'fetch_file.dart';
 
 void main() {
   runApp(const MyApp());
--- eight_lesson/lib/my_home_page.dart
+++ eight_lesson/lib/my_home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'fetch_file.dart';
+
+String fileName = "";
+String fileData = "";
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -9,11 +13,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          TextField(
+            controller: myController,
+            decoration: InputDecoration(
+              hintText: "Введите имя файла",
+              border: const OutlineInputBorder(),
+              suffixIcon: Container(
+                margin: const EdgeInsets.all(0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    minimumSize: const Size(100, 60),
+                    backgroundColor: Colors.black,
+                  ),
+                  child: const Text(
+                    "Найти",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    fileName = myController.text;
+                    fetchFileFromAssets("assets/$fileName.txt").then((value) {
+                      fileData = value;
+                      print(fileData);
+                    }).catchError((err) {
+                      fileName = "Файл не найден";
+                    });
+                    setState(() {});
+                  },
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: Text(
+              fileName,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Text(fileData),
+        ]),
       ),
     );
   }
