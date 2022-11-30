9954deee4bc670e1745da179ab6a520c2727d4f8
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2022-11-30 10:55:42 +0300

    new lesson

---
+++ eight_lesson/lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:eight_lesson/my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
