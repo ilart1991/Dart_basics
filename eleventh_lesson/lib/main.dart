e216db4cf5d17d03d13cf3c91350cec4d99bb657
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2022-12-07 14:45:58 +0300

    complete

--- eleventh_lesson/lib/main.dart
+++ eleventh_lesson/lib/main.dart
@@ -1,5 +1,67 @@
 import 'package:flutter/material.dart';
 
+List spaces = [
+  {
+    "image": const AssetImage("assets/images/space0.jpg"),
+    "uuid": "7b25e714",
+    "name": "space0"
+  },
+  {
+    "image": const AssetImage("assets/images/space1.jpg"),
+    "uuid": "7b25e114",
+    "name": "space1"
+  },
+  {
+    "image": const AssetImage(
+      "assets/images/space2.jpg",
+    ),
+    "uuid": "7b25e71774",
+    "name": "space2"
+  },
+  {
+    "image": const AssetImage(
+      "assets/images/space3.jpg",
+    ),
+    "uuid": "7bgg25e714",
+    "name": "space2"
+  },
+  {
+    "image": const AssetImage(
+      "assets/images/space4.jpg",
+    ),
+    "uuid": "7b25gge714",
+    "name": "space2"
+  },
+  {
+    "image": const AssetImage(
+      "assets/images/space5.jpg",
+    ),
+    "uuid": "711b25e714",
+    "name": "space2"
+  },
+  {
+    "image": const AssetImage(
+      "assets/images/space6.jpg",
+    ),
+    "uuid": "7b25e71114",
+    "name": "space2"
+  },
+  {
+    "image": const AssetImage(
+      "assets/images/space7.jpg",
+    ),
+    "uuid": "732b25e714",
+    "name": "space2"
+  },
+  {
+    "image": const AssetImage(
+      "assets/images/space8.jpg",
+    ),
+    "uuid": "7b2512e714",
+    "name": "space2"
+  },
+];
+
 void main() {
   runApp(const MyApp());
 }
@@ -31,12 +93,106 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView.builder(
+          itemCount: spaces.length,
+          itemBuilder: (context, index) {
+            return Card(
+              semanticContainer: true,
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              elevation: 5,
+              margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
+              child: Stack(
+                alignment: AlignmentDirectional.bottomCenter,
+                children: <Widget>[
+                  Container(
+                      height: 130,
+                      child: Hero(
+                        tag: spaces[index]["uuid"],
+                        child: FadeInImage(
+                          placeholder:
+                              const AssetImage("assets/basketball.gif"),
+                          image: spaces[index]["image"],
+                          height: 200,
+                          width: 1000,
+                          fit: BoxFit.none,
+                        ),
+                      )),
+                  Positioned(
+                      top: 100,
+                      child: Container(
+                          width: 400,
+                          height: 80,
+                          color: Colors.white,
+                          child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Hero(
+                                tag: spaces[index]["uuid"] + "123",
+                                child: Text(
+                                  spaces[index]["name"],
+                                  textAlign: TextAlign.center,
+                                ),
+                              )))),
+                  Positioned(
+                    right: 60,
+                    bottom: 10,
+                    child: MaterialButton(
+                      color: Colors.blue,
+                      minWidth: 45,
+                      height: 45,
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => DetailPage(
+                              image: spaces[index]["image"],
+                              uuid: spaces[index]["uuid"],
+                              name: spaces[index]["name"]),
+                        ));
+                      },
+                      child: const Icon(
+                        Icons.arrow_right_alt_rounded,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ));
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  const DetailPage(
+      {super.key, required this.image, required this.uuid, required this.name});
+  final AssetImage image;
+  final String uuid;
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
       body: Center(
-        child: Text("hop hey la la ley"),
-      ),
+          child: Column(
+        children: [
+          Hero(
+            tag: uuid,
+            child: FadeInImage(
+              height: 400,
+              image: image,
+              placeholder: const AssetImage("assets/basketball.gif"),
+            ),
+          ),
+          Hero(
+            tag: name,
+            child: Text(name),
+          ),
+        ],
+      )),
     );
   }
 }
