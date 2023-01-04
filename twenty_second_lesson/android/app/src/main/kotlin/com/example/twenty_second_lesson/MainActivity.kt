1898fbfa09d55900f051a212554ff387f21afe13
Артем Ильинский <42186657+ilart1991@users.noreply.github.com>
2023-01-04 09:48:27 +0300

    new with cwidget

---
+++ twenty_second_lesson/android/app/src/main/kotlin/com/example/twenty_second_lesson/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twenty_second_lesson
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ twenty_second_lesson/lib/helper_widgets.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget appBar() {
+  return AppBar(
+    title: const Text(
+      "New Default Title",
+    ),
+  );
+}
